0e37186c6ed19d02d06b1c68d63b26c66222b68e
MatchaApril <820040980@qq.com>
2022-02-02 10:14:50 +0800

    2022.2.2

--- src/main/java/com/example/springbootstudy02/controller/EmployeeController.java
+++ src/main/java/com/example/springbootstudy02/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,4 +43,31 @@ public class EmployeeController {
         employeeDao.add(employee);    //调用底层业务方法保存员工信息(这方法是在EmployeeDao类里自己写的)
         return "redirect:/emps";   //重定向（重定向是：A->B改为A->C，转发是A->B->C）
     }
+
+    //去员工的修改页面
+    @GetMapping("/emp/{id}")
+    public String toUpdateEmp(@PathVariable("id")Integer id,Model model){
+        //查出原来的数据
+        Employee employee = employeeDao.getEmployeeById(id);
+        model.addAttribute("emp",employee);
+        //需要查出所有部门的信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+
+        return "emp/update";
+    }
+
+    @PostMapping("/updateEmp")
+    public String updateEmp(Employee employee){
+        employeeDao.add(employee);
+        return "redirect:/emps";
+    }
+
+    //删除员工
+    @GetMapping("/delemp/{id}")
+    public  String deleteEmp(@PathVariable("id")Integer id,Model model){
+        employeeDao.delete(id);
+        return "redirect:/emps";
+    }
+
 }
